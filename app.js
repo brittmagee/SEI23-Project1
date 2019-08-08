2c2727dfef6133738dad6fc3cc0013bf62a4a564
brittmagee <brittgmagee@gmail.com>
2019-08-08 14:28:57 -0400

    Saving first portion of easy array sequence

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ function planetBeep(i){
         mercuryBeep.play();
         document.getElementById("mercury").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
         let meGlow = document.getElementById("mercury");
-        meGlow.onclick = setInterval(removeGlow, 300);
+        meGlow.onclick = setTimeout(removeGlow, 300);
     }else if(i == "v"){
         venusBeep.play();
         document.getElementById("venus").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
         let vGlow = document.getElementById("venus");
-        vGlow.onclick = setInterval(removeGlow, 300);
+        vGlow.onclick = setTimeout(removeGlow, 300);
     }else if(i == "e"){
         earthBeep.play();
         document.getElementById("earth").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
         let eGlow = document.getElementById("earth");
-        eGlow.onclick = setInterval(removeGlow, 300);
+        eGlow.onclick = setTimeout(removeGlow, 300);
     }else if(i == "ma"){
         marsBeep.play();
         document.getElementById("mars").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
         let maGlow = document.getElementById("mars");
-        maGlow.onclick = setInterval(removeGlow, 300);
+        maGlow.onclick = setTimeout(removeGlow, 300);
     }
 }
 
@@ -36,9 +36,48 @@ function removeGlow(){
     document.getElementById("mars").style.boxShadow = null;
 }
 
+let easy = ["earth","venus","mars","mercury","mercury","earth","mercury","mars","venus","venus"];
+
+function mercurySequence (){
+    document.getElementById("mercury").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+    mercuryBeep.play();
+    let meGlow = document.getElementById("mercury");
+    meGlow = setTimeout(removeGlow, 300);
+}
+
+
+function venusSequence (){
+    venusBeep.play();
+    document.getElementById("venus").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+    let vGlow = document.getElementById("venus");
+    vGlow = setTimeout(removeGlow, 300);
+}
+
+
+// earthBeep.play();
+// document.getElementById("earth").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+// let eGlow = document.getElementById("earth");
+// eGlow.onclick = setInterval(removeGlow, 300);
+
+// marsBeep.play();
+// document.getElementById("mars").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+// let maGlow = document.getElementById("mars");
+// maGlow.onclick = setInterval(removeGlow, 300);
+
+
+
 //select levels
 document.getElementById("easy").addEventListener("click", function(){
     console.log("works!")
+    for(let i = 0; i < easy.length; i++){
+        console.log(easy[i]);
+        if(easy[i]== "mercury"){
+            setTimeout(mercurySequence, 300*i);
+        } else if (easy[i]== "venus"){
+            setTimeout(venusSequence, 300*i); 
+        }
+    }
+
 });
 
 document.getElementById("medium").addEventListener("click", function(){
@@ -49,3 +88,34 @@ document.getElementById("hard").addEventListener("click", function(){
     console.log("woohoo!")
 });
 
+//function for easy level (10)
+
+let planets = ["mercury","venus","earth","mars"]
+let easyLevel = []
+
+// {
+//     nextPlanet = Math.floor(Math.random()*5)
+//     console.log(easy[i]);
+//     document.getElementById("mercury").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+//     let meGlow = document.getElementById("mercury");
+//     meGlow.onclick = setInterval(removeGlow, 300);
+//     easyLevel.push(nextPlanet)
+// }
+
+
+// function easyLevel (){
+//     for(let i = 0; i < easy.length; i++){
+//         console.log(easy[i]);
+//         document.getElementById("mercury").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+//         let meGlow = document.getElementById("mercury");
+//         meGlow.onclick = setInterval(removeGlow, 300);
+//     }
+//     console.log("works")
+// }
+
+// //function for medium level(15)
+// let medium = ["mars","venus","mars","mercury","mercury","earth","venus",'Mars',"earth","mars","mercury","mercury","earth","venus","mars"];
+
+
+// //function for hard level(20)
+// let hard = ["venus","mercury","venus","mars","earth","mars","mercury","earth","venus","venus","mercury","mars","earth","mercury","mars","venus","venus","mercury","earth","mars"];
