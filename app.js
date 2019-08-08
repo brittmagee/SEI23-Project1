99afadffc4955a7910c627f7a36938197303a838
brittmagee <brittgmagee@gmail.com>
2019-08-08 10:59:57 -0400

    Set the onclick planet glow function to a setInterval and null

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ let mercuryBeep = new Audio ("audio/mercury.mp3")
 let venusBeep = new Audio ("audio/venus.mp3")
 let earthBeep = new Audio ("audio/earth.mp3")
 let marsBeep = new Audio ("audio/mars.mp3")
+
 function planetBeep(i){
     if(i == "me"){
         mercuryBeep.play();
         document.getElementById("mercury").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+        let meGlow = document.getElementById("mercury");
+        meGlow.onclick = setInterval(removeGlow, 300);
     }else if(i == "v"){
         venusBeep.play();
         document.getElementById("venus").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+        let vGlow = document.getElementById("venus");
+        vGlow.onclick = setInterval(removeGlow, 300);
     }else if(i == "e"){
         earthBeep.play();
         document.getElementById("earth").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+        let eGlow = document.getElementById("earth");
+        eGlow.onclick = setInterval(removeGlow, 300);
     }else if(i == "ma"){
         marsBeep.play();
         document.getElementById("mars").style.boxShadow = "0px 0px 30px 2px rgb(105, 255, 85), 0px 0px 30px 2px rgb(105, 255, 85) inset";
+        let maGlow = document.getElementById("mars");
+        maGlow.onclick = setInterval(removeGlow, 300);
     }
 }
 
+//function to have glow removed after .3s 
+function removeGlow(){
+    document.getElementById("mercury").style.boxShadow = null;
+    document.getElementById("venus").style.boxShadow = null;
+    document.getElementById("earth").style.boxShadow = null;
+    document.getElementById("mars").style.boxShadow = null;
+}
