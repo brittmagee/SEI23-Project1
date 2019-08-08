14d4a7e7e81653a7083c91f95055dadda6537380
brittmagee <brittgmagee@gmail.com>
2019-08-07 22:14:38 -0400

    Testing the planet glow effect

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ let marsBeep = new Audio ("audio/mars.mp3")
 function planetBeep(i){
     if(i == "me"){
         mercuryBeep.play();
+        document.getElementById("mercury").style.border = "1px solid #4195fc";
     }else if(i == "v"){
         venusBeep.play();
+        document.getElementById("venus").style.border = "1px solid #4195fc";
     }else if(i == "e"){
         earthBeep.play();
     }else if(i == "ma"){
